bbf97740d69c8b98c5379d8dd2b50e42906edbe0
Gert Geidel <gert.geidel@infopark.de>
2015-04-21 16:18:40 +0200

    SDK 0.60.0, new version

--- app/models/headline_widget.rb
+++ app/models/headline_widget.rb
@@ -1,4 +1,11 @@
 class HeadlineWidget < Widget
+  attribute :headline, :string
+  attribute :anchor, :string
+  attribute :type, :enum, values: ['h1','h2','h3','h4','h5','h6']
+  attribute :size, :enum, values: ['large','medium','small','text']
+  attribute :style, :enum, values: ['normal','bold','italic','bold-italic']
+  attribute :alignment, :enum, values: ['center','left','right']
+
   def css_class
     [self.size, self.css_style, self.alignment].join(" ")
   end
